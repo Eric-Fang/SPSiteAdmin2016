76746b3b71feb0853a96b90570324ef43e585644
0efang <0efang@PDSPC01>
2016-10-20 14:00:30 +1100

    fix minor bugs

--- Form1.cs
+++ Form1.cs
@@ -821,7 +821,12 @@ namespace SPSiteAdmin2016
             string strLine = string.Empty;
             textBoxPSScript.Text = string.Empty;
 
-            string strManagedPathDest = ((KeyValuePair<string, string>)comboBoxCrossWebAppManagedPath.SelectedItem).Key;
+            string strManagedPathDest = string.Empty;
+            if (comboBoxCrossWebAppManagedPath.SelectedItem != null)
+            {
+                strManagedPathDest = ((KeyValuePair<string, string>)comboBoxCrossWebAppManagedPath.SelectedItem).Key;
+            }
+            
             string strContentDB = ((KeyValuePair<string, string>)comboBoxCrossWebAppContentDBDest.SelectedItem).Value;
 
             string strSourceWebAppGUID = ((KeyValuePair<string, string>)comboBoxCrossWebAppSource.SelectedItem).Key;
@@ -1423,7 +1428,14 @@ namespace SPSiteAdmin2016
             if (comboBoxCrossWebAppContentDBDest.SelectedItem == null)
                 return;
 
-            RefreshPSScriptTextBoxCrossWebApp();
+            if (_dictPSScriptCrossWebApp.Count > 0)
+            {
+                // RefreshPSScriptTextBoxInWebApp();
+                buttonClearPSScript_Click(null, null);
+                comboBoxCrossWebAppContentDBSource_SelectedIndexChanged(null, null);
+            }
+
+            // RefreshPSScriptTextBoxCrossWebApp();
 
             string strKeyCrossWebAppDest = ((KeyValuePair<string, string>)comboBoxCrossWebAppDest.SelectedItem).Key;
             string strKeyCrossWebAppContentDBDest = ((KeyValuePair<string, string>)comboBoxCrossWebAppContentDBDest.SelectedItem).Key;
